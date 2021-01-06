b7ede342747a753f22bdf494c1b03becbb810c5f
matus gallik <matusgallik008@gmail.com>
2021-01-06 18:54:19 +0100

    Readme

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 import Parallax from "./components/Parallax";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Helmet} from "react-helmet";
 
 function App() {
     return (
-        <Parallax/>
+        <div>
+            <Helmet>
+                <meta name="theme-color" content="#000000"/>
+            </Helmet>
+            <Parallax/>
+        </div>
     );
 }
 
